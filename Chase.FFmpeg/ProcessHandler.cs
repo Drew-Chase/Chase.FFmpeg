fc26eb118f90b5865c7fc5c4f7e3efd110e91e4d
Drew Chase <dcmanproductions@gmail.com>
2022-11-08 18:10:35 -0500

    Added standard and downloader class libraries

---
+++ Chase.FFmpeg/ProcessHandler.cs
@@ -0,0 +1,55 @@
+﻿using Chase.FFmpeg.Downloader;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Chase.FFmpeg;
+
+internal static class ProcessHandler
+{
+    public static void ExecuteFFmpeg(string arguments, DataReceivedEventHandler? data_handler, EventHandler? exited)
+    {
+        Process process = new()
+        {
+            StartInfo = new()
+            {
+                FileName = FFmpegDownloader.Instance.FFmpegExecutable,
+                Arguments = arguments,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+            },
+            EnableRaisingEvents = true,
+        };
+
+        if (data_handler != null)
+            process.ErrorDataReceived += data_handler;
+        if (exited != null)
+            process.Exited += exited;
+        process.Start();
+        process.BeginErrorReadLine();
+        process.WaitForExit();
+        process.Close();
+    }
+    public static void ExecuteFFprobe(string arguments, DataReceivedEventHandler? data_handler, EventHandler? exited)
+    {
+        Process process = new()
+        {
+            StartInfo = new()
+            {
+                FileName = FFmpegDownloader.Instance.FFprobeExecutable,
+                Arguments = arguments,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+            },
+            EnableRaisingEvents = true,
+        };
+
+        if (data_handler != null)
+            process.OutputDataReceived += data_handler;
+        if (exited != null)
+            process.Exited += exited;
+        process.Start();
+        process.BeginErrorReadLine();
+        process.WaitForExit();
+        process.Close();
+    }
+}
