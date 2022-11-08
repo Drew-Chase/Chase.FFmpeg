9b98c10b8b3feded786e6d4b7e5b4c13ba2cbd94
Drew Chase <dcmanproductions@gmail.com>
2022-11-08 18:09:22 -0500

    Can parse os specific ffmpeg urls

---
+++ Chase.FFmpeg.Downloader/Networking/FFUrlParser.cs
@@ -0,0 +1,32 @@
+﻿using Chase.FFmpeg.Downloader.Environment;
+using Newtonsoft.Json.Linq;
+
+namespace Chase.FFmpeg.Downloader.Networking;
+
+internal sealed class FFUrlParser
+{
+    public readonly static FFUrlParser Instance = Instance ??= new();
+    public readonly Uri FFmpeg, FFprobe;
+    public readonly string Version;
+    private FFUrlParser()
+    {
+        JObject json = GetJson();
+
+        Version = (string)json["version"];
+        FFmpeg = new((string)json["bin"][OS_Provider.Name]["ffmpeg"]);
+        FFprobe = new((string)json["bin"][OS_Provider.Name]["ffprobe"]);
+    }
+
+    private JObject GetJson()
+    {
+        using HttpClient client = new();
+        using HttpResponseMessage message = client.GetAsync("https://ffbinaries.com/api/v1/version/latest").Result;
+
+        if (message.IsSuccessStatusCode)
+        {
+            return JObject.Parse(message.Content.ReadAsStringAsync().Result);
+        }
+        throw new System.Net.WebException($"FFBinaries api returned with status code {message.StatusCode}");
+    }
+
+}
