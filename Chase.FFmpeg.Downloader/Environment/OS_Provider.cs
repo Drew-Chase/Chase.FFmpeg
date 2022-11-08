def1c84cbbcda9a80a1972515bb2138fda6c4359
Drew Chase <dcmanproductions@gmail.com>
2022-11-08 18:08:57 -0500

    Added ability to get current operating system features

---
+++ Chase.FFmpeg.Downloader/Environment/OS_Provider.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Chase.FFmpeg.Downloader.Environment;
+
+/// <summary>
+/// Gives utility variables for the current operating system
+/// </summary>
+public static class OS_Provider
+{
+
+    /// <summary>
+    /// The ffmpeg friendly name for the systems cpu architecture and Operating System name
+    /// </summary>
+    public static string Name
+    {
+        get
+        {
+            StringBuilder name = new();
+
+            if (OperatingSystem.IsLinux())
+            {
+                name.Append("linux");
+                if (IsARM64)
+                {
+                    name.Append("-arm64");
+                }
+                else
+                {
+                    // ARM hardware float / ARM 32 bit processors
+                    name.Append("-armhf");
+                }
+            }
+            else if (OperatingSystem.IsWindows())
+            {
+                name.Append("windows-64");
+            }
+            else if (OperatingSystem.IsMacOS())
+            {
+                name.Append("osx-64");
+            }
+
+            return name.ToString();
+        }
+    }
+
+    /// <summary>
+    /// If the processor is ARM based or not
+    /// </summary>
+    public static bool IsARM => IsARM64 || IsARM32;
+    /// <summary>
+    /// If the processor is ARM 64 bit or not
+    /// </summary>
+    public static bool IsARM64 => RuntimeInformation.ProcessArchitecture == Architecture.Arm64;
+    /// <summary>
+    /// If the processor is ARM 32 bit or not
+    /// </summary>
+    public static bool IsARM32 => RuntimeInformation.ProcessArchitecture == Architecture.Arm64;
+    /// <summary>
+    /// If the processor is AMD 64 bit
+    /// </summary>
+    public static bool Is64 => RuntimeInformation.ProcessArchitecture == Architecture.X64;
+    /// <summary>
+    /// If the processor is Intel i386 32 bit processor
+    /// </summary>
+    public static bool Is32 => RuntimeInformation.ProcessArchitecture == Architecture.X86;
+
+}
