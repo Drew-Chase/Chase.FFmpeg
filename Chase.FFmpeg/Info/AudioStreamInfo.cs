84fd4b2b83714e6dc42eef0a679a6c45230275c7
Drew Chase <dcmanproductions@gmail.com>
2022-11-09 21:24:50 -0500

    Added audio stream info

---
+++ Chase.FFmpeg/Info/AudioStreamInfo.cs
@@ -0,0 +1,42 @@
+﻿namespace Chase.FFmpeg.Info;
+
+/// <summary>
+/// Stores information about the audio stream
+/// </summary>
+public struct AudioStreamInfo
+{
+    /// <summary>
+    /// The codec that the audio stream was encoded with
+    /// </summary>
+    public string Codec { get; private set; }
+    /// <summary>
+    /// The sample rate of the audio stream
+    /// </summary>
+    public uint SampleRate { get; private set; }
+    /// <summary>
+    /// The sample format of the audio stream
+    /// </summary>
+    public string SampleFormat { get; private set; }
+    /// <summary>
+    /// The channel layout of the audio stream
+    /// </summary>
+    public float ChannelLayout { get; private set; }
+    /// <summary>
+    /// The size in bytes of the audio stream
+    /// </summary>
+    public uint Size { get; private set; }
+    /// <summary>
+    /// The number of channels in the audio stream
+    /// </summary>
+    public uint Channels { get; private set; }
+
+    internal AudioStreamInfo(string codec, uint sampleRate, string sampleFormat, float channelLayout, uint size, uint channels)
+    {
+        Codec = codec;
+        SampleRate = sampleRate;
+        SampleFormat = sampleFormat;
+        ChannelLayout = channelLayout;
+        Size = size;
+        Channels = channels;
+    }
+}
