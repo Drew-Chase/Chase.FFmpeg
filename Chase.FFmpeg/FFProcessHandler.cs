c37a81458f6422825ed92b35b3bff22268f76e44
Drew Chase <dcmanproductions@gmail.com>
2022-11-10 16:32:40 -0500

    Added update event

--- Chase.FFmpeg/FFProcessHandler.cs
+++ Chase.FFmpeg/FFProcessHandler.cs
@@ -1,25 +1,33 @@
 ﻿using Chase.FFmpeg.Downloader;
+using Chase.FFmpeg.Events;
+using Chase.FFmpeg.Info;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace Chase.FFmpeg;
 
 public static class FFProcessHandler
 {
+
+
     /// <summary>
     /// Executes a command to the ffmpeg executable
     /// </summary>
     /// <param name="arguments">The ffmpeg arguments</param>
     /// <param name="data_handler">Executes when ffmpeg outputs a line to the console</param>
     /// <param name="exited">Executes when ffmpeg process stops running</param>
-    public static void ExecuteFFmpeg(string arguments, DataReceivedEventHandler? data_handler, EventHandler? exited)
+    public static void ExecuteFFmpeg(string arguments, MediaInfo? info = null, DataReceivedEventHandler? data_handler = null, EventHandler? exited = null, EventHandler<FFProcessUpdateEventArgs>? updated = null)
     {
+        float Percentage = 0f;
+        uint FramesProcessed = 0;
+        float Speed = 0f;
+        float AverageBitrate = 0f;
         Process process = new()
         {
             StartInfo = new()
             {
                 FileName = FFmpegDownloader.Instance.FFmpegExecutable,
                 Arguments = arguments,
-                CreateNoWindow = true,
                 RedirectStandardError = true,
                 RedirectStandardOutput = true,
             },
@@ -33,6 +41,63 @@ public static class FFProcessHandler
         }
         if (exited != null)
             process.Exited += exited;
+
+
+        if (info != null)
+        {
+            process.ErrorDataReceived += (s, e) =>
+            {
+                string? content = e.Data;
+                if (!string.IsNullOrWhiteSpace(content))
+                {
+                    if (content.StartsWith("frame="))
+                    {
+                        MatchCollection regexMatches = Regex.Matches(content, "((?!\\s).)*[A-z]=((?!(\\s[A-z])).)*");
+                        foreach (object matchObject in regexMatches)
+                        {
+                            try
+                            {
+                                string? t = matchObject.ToString().Trim().Replace(" ", "");
+                                string[] parts = t.Split('=');
+                                if (parts.Length == 2)
+                                {
+                                    switch (parts[0])
+                                    {
+                                        case "frame":
+                                            FramesProcessed = Convert.ToUInt32(parts[1]);
+                                            break;
+                                        case "fps":
+                                            break;
+                                        case "size":
+                                            break;
+                                        case "time":
+                                            break;
+                                        case "bitrate":
+                                            string d = parts[1].Replace("kbits/s", "");
+                                            AverageBitrate = Convert.ToSingle(parts[1].Replace("kbits/s", ""));
+                                            break;
+                                        case "speed":
+                                            Speed = Convert.ToSingle(parts[1].Trim('x'));
+                                            break;
+                                        default: break;
+                                    }
+                                }
+                            }
+                            catch { }
+                        }
+
+                        Percentage = ((float)FramesProcessed / info.VideoStream.Frames) * 100;
+                        updated?.Invoke(null, new()
+                        {
+                            Speed = Speed,
+                            AverageBitrate = AverageBitrate,
+                            FramesProcessed = FramesProcessed,
+                            Percentage = Percentage,
+                        });
+                    }
+                }
+            };
+        }
         process.Start();
         process.BeginErrorReadLine();
         process.BeginOutputReadLine();
