c169efa75dfe817beadf5227d28b1d476a0d0a24
Drew Chase <dcmanproductions@gmail.com>
2022-11-09 21:25:27 -0500

    Added ability to find supported hardware acceleration method

---
+++ Chase.FFmpeg/Info/SupportedHardwareAccelerationMethods.cs
@@ -0,0 +1,24 @@
+﻿namespace Chase.FFmpeg.Info;
+
+public class SupportedHardwareAccelerationMethods
+{
+    public static SupportedHardwareAccelerationMethods Instance = Instance ??= new();
+    public string[] Methods { get; private set; }
+
+    private SupportedHardwareAccelerationMethods()
+    {
+        List<string> methods = new();
+        FFProcessHandler.ExecuteFFmpeg("-hide_banner -hwaccels", (s, e) =>
+        {
+            string? content = e.Data;
+            if (!string.IsNullOrWhiteSpace(content))
+            {
+                if (!content.ToLower().StartsWith("hardware acceleration methods"))
+                {
+                    methods.Add(content);
+                }
+            }
+        }, null);
+        Methods = methods.ToArray();
+    }
+}
