bc7696598eabd588a1743d015176a88a69073256
Drew Chase <dcmanproductions@gmail.com>
2022-11-10 16:31:38 -0500

    Added update event arguments

---
+++ Chase.FFmpeg/Events/FFProcessUpdateEventArgs.cs
@@ -0,0 +1,9 @@
+﻿namespace Chase.FFmpeg.Events;
+
+public class FFProcessUpdateEventArgs : EventArgs
+{
+    public float Percentage { get; set; }
+    public uint FramesProcessed { get; set; }
+    public float Speed { get; set; }
+    public float AverageBitrate { get; set; }
+}
