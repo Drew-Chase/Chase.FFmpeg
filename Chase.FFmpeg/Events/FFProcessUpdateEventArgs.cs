875d134f50f546967bf74f39ea4f9dfd28676906
Drew Chase <dcmanproductions@gmail.com>
2022-11-10 21:56:24 -0500

    added documentation

--- Chase.FFmpeg/Events/FFProcessUpdateEventArgs.cs
+++ Chase.FFmpeg/Events/FFProcessUpdateEventArgs.cs
@@ -2,8 +2,20 @@
 
 public class FFProcessUpdateEventArgs : EventArgs
 {
+    /// <summary>
+    /// The percentage of the video has been processed
+    /// </summary>
     public float Percentage { get; set; }
+    /// <summary>
+    /// The number of frames already processed
+    /// </summary>
     public uint FramesProcessed { get; set; }
+    /// <summary>
+    /// The speed that the video is processing at
+    /// </summary>
     public float Speed { get; set; }
+    /// <summary>
+    /// The bitrate that the video is being processed at
+    /// </summary>
     public float AverageBitrate { get; set; }
 }
