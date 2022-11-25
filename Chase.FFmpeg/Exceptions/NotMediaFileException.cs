9892e2697ea8882b2749bdcc2d822a21b81d6ab5
Drew Chase <dcmanproductions@gmail.com>
2022-11-24 21:00:19 -0500

    Added exception for if a file is not a media file

---
+++ Chase.FFmpeg/Exceptions/NotMediaFileException.cs
@@ -0,0 +1,12 @@
+﻿namespace Chase.FFmpeg.Exceptions;
+
+/// <summary>
+/// When a file is not a media file!
+/// </summary>
+public class NotMediaFileException : IOException
+{
+    internal NotMediaFileException(string file) : base($"File is not a media file: {file}")
+    {
+
+    }
+}
