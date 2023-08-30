5c2dcf4b843748117ae8b1df8add365163ba3b19
Drew Chase <dcmanproductions@gmail.com>
2023-08-30 14:00:54 -0400

    added class to handle windows

---
+++ Chase.FFmpeg.Capture/Utilities/WindowHandler.cs
@@ -0,0 +1,19 @@
+﻿// LFInteractive LLC. - All Rights Reserved
+using System.Diagnostics;
+
+namespace Chase.FFmpeg.Capture.Utilities;
+
+public static class WindowHandler
+{
+    public static string[] GetWindowTitles()
+    {
+        List<string> result = new();
+        Process[] processes = Process.GetProcesses();
+        foreach (Process process in processes)
+        {
+            if (!string.IsNullOrEmpty(process.MainWindowTitle))
+                result.Add(process.MainWindowTitle);
+        }
+        return result.ToArray();
+    }
+}
