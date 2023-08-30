b75137c9e3fe85c6c922e403d3e0b32b2d4d466d
Drew Chase <dcmanproductions@gmail.com>
2023-08-30 19:53:26 -0400

    added input type enum

---
+++ Chase.FFmpeg.Capture/Data/InputType.cs
@@ -0,0 +1,15 @@
+﻿/*
+    Chase FFmpeg - LFInteractive LLC. 2021-2024
+    Chase FFmpeg is a ffmpeg wrapper for c#. Includes the ability to download, execute and manipulate ffmpeg, ffprobe and ffplay.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+namespace Chase.FFmpeg.Capture.Data;
+
+public enum InputType
+{
+    None,
+    Video,
+    Audio,
+}
