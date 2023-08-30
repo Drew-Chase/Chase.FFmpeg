0d2781aa06f3bc143fb5dc576f7720344f95643c
Drew Chase <dcmanproductions@gmail.com>
2023-08-30 19:53:46 -0400

    added point struct

---
+++ Chase.FFmpeg.Capture/Data/Point.cs
@@ -0,0 +1,23 @@
+﻿/*
+    Chase FFmpeg - LFInteractive LLC. 2021-2024
+    Chase FFmpeg is a ffmpeg wrapper for c#. Includes the ability to download, execute and manipulate ffmpeg, ffprobe and ffplay.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+using System.Runtime.InteropServices;
+
+namespace Chase.FFmpeg.Capture.Utilities;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct Point
+{
+    public int x;
+    public int y;
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
