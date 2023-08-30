86a73f1cbdd0fb612a47336077eb772e9d1cc2b8
Drew Chase <dcmanproductions@gmail.com>
2023-08-30 19:52:13 -0400

    added Invalid Operating System exception

---
+++ Chase.FFmpeg/Exceptions/InvalidOperatingSystemException.cs
@@ -0,0 +1,17 @@
+﻿/*
+    Chase FFmpeg - LFInteractive LLC. 2021-2024
+    Chase FFmpeg is a ffmpeg wrapper for c#. Includes the ability to download, execute and manipulate ffmpeg, ffprobe and ffplay.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+using System.Runtime.InteropServices;
+
+namespace Chase.FFmpeg.Exceptions;
+
+public class InvalidOperatingSystemException : Exception
+{
+    public InvalidOperatingSystemException() : base($"Operating System not supported: {RuntimeInformation.OSDescription}")
+    {
+    }
+}
