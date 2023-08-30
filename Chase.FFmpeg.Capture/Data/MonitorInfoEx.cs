9f93076f04e83d462e355d5124edbf49073ee707
Drew Chase <dcmanproductions@gmail.com>
2023-08-30 19:53:41 -0400

    added Monitor Info struct

---
+++ Chase.FFmpeg.Capture/Data/MonitorInfoEx.cs
@@ -0,0 +1,22 @@
+﻿/*
+    Chase FFmpeg - LFInteractive LLC. 2021-2024
+    Chase FFmpeg is a ffmpeg wrapper for c#. Includes the ability to download, execute and manipulate ffmpeg, ffprobe and ffplay.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+using System.Runtime.InteropServices;
+
+namespace Chase.FFmpeg.Capture.Utilities;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct MonitorInfoEx
+{
+    public int cbSize;
+    public Rect rcMonitor;
+    public Rect rcWork;
+    public uint dwFlags;
+
+    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+    public string szDevice;
+}
